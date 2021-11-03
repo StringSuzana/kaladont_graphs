6150c2cd67932ca0e01ec96809067771e4ee9153
suzana <santol2@racunarstvo.hr>
2021-11-03 12:58:31 +0100

    oh good lord

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import string
 import itertools
 import ast
 from collections import deque
+import random
+import time
 
 graph = {}
 
@@ -85,6 +87,7 @@ def dfs(graph, start, visited=set()):
     stack.append(start)  # Push starting node to stack
 
     while len(stack) > 0:
+
         current = stack.pop()  # From the TOP of the stack
         visited_list.append(current)
         destinations = graph[current]
@@ -96,27 +99,58 @@ def dfs(graph, start, visited=set()):
     print('========================END========================')
 
 
-def dfs_modified(rand_sort, start, visited_list=[]):
-    # visited_list.append(start)
+def get_weight(target_set: set(), visited_list: []) -> int:
+    # print(len(target_set))
+    target_set.discard(set(visited_list))
+    # print(len([x for x in target_set if x not in visited_list]))  # Also ok
+    return len(target_set)
+
+
+def dfs_modified(start, visited_list=[]):
     stack = deque()  # Init stack for a while loop
     stack.append(start)  # Push starting node to stack
 
     while len(stack) > 0:
+
         current = stack.pop()  # From the TOP of the stack
-        if len(visited_list) > 0:
-            if visited_list[len(visited_list) - 1][-2:] != current[0:2]:
-                if len(visited_list) > 1:
-                    return visited_list[:-1]
 
         if current not in visited_list:
-            visited_list.append(current)
-            destinations = graph[current]
-            # print(current)
+            if len(
+                    visited_list) > 0:  # Ovo znaci da je krenuo na novu granu traziti u dubinu a to sam odlucila zanemariti
+                if (visited_list[len(visited_list) - 1][-2:] != current[0:2]) & (current != start):
+                    return visited_list
+            # Ako nije krenuo na novu granu nego nastavlja put niz prvu niz koju je krenuo:
 
-            for dest in sorted(destinations, reverse=rand_sort):
-                if dest not in visited_list:
-                    stack.append(dest)
-    return []
+            # Osim ako se trenutna rijec ne moze povezati na barem dvije
+            if len(graph.get(current) - set(visited_list)) < 2:
+                continue
+
+            visited_list.append(current)
+            #print(len(visited_list))
+            destinations = graph[current]  # Sve nadovezive rijeci od TRENUTNO VISITED rijeci:
+            destination_half_sample = random.sample(list(destinations), k=(
+                int(len(destinations) / 2)))  # mejbi pretvorit u listu destinations?
+            if len(destination_half_sample) < 100:
+                destination_half_sample = destinations
+            # print(current)
+            # for dest in sorted(destinations, reverse=rand_sort): 200-400
+            # Sortirat destinations po tome koliko oni u graph[dest] imaju spojnih destinacija (koje nisu u visited? => to je ekstra effort)
+            # wanted = lambda w: len(graph.get(w))
+            # lambda w: len(graph.get(w).discard(set(visited_list)))
+            # lambda w: len([x for x in graph.get(w) if x not in visited_list]) # ULTRA sporo 0.59000253...0.19998931
+            # lambda w: get_weight(graph.get(w), visited_list) 0.016001701...0.0130000114
+            # lambda w: len(graph.get(w)- set(visited_list)) 0.088975191 ...0.058000326156
+            # before_sort = time.time()
+            test = sorted(destination_half_sample, key=lambda w: len(graph.get(w)),
+                          reverse=False)
+            # after_sort = time.time()
+            # print(f"Sort time: {after_sort - before_sort}")
+
+            # bla = [dest for dest in test[int(len(test) / 2):] if dest not in visited_list]
+            for a in test:
+                stack.append(a)
+
+    return visited_list
 
 
 if __name__ == '__main__':
@@ -137,19 +171,36 @@ if __name__ == '__main__':
         GAME_OVER = False
         starting_word = every_word
         visited = []
-        print(starting_word)
-        toggle: bool = False
+        # visited.append(starting_word) NO BECAUSE OF THE DFS FUNCTION IMPLEMENTATION
+        print(f"STARTING WITH NEW WORD: {starting_word}")
+        guarding_value=0
+        count = 0
+        dfs_result_visited = []
         while not GAME_OVER:
-            toggle = not toggle
-            new_visited = dfs_modified(toggle, starting_word, visited)
-            if len(new_visited) > 0:
-                starting_word = new_visited.pop()
-                if len(new_visited) > 0:
-                    visited.extend(new_visited)
-
+            print(f"Visited len BEFORE entering dfs func: {len(visited)}")
+            dfs_result_visited = dfs_modified(starting_word, visited[:])
+            if len(dfs_result_visited) > (len(
+                    visited) + 1):  # Zbog toga sto sam sigurna da cu barem starting word staviti u visited unutar funkcije dfs
+                dfs_result_visited.pop()
+                dfs_result_visited.pop()
+                dfs_result_visited.pop()
+                dfs_result_visited.pop()
+                dfs_result_visited.pop()
+                dfs_result_visited.pop()
+                dfs_result_visited.pop()
+                dfs_result_visited.pop()
+                starting_word = dfs_result_visited.pop()
+                visited = dfs_result_visited[:]
+                print(f"Change starting_word to: {starting_word}")
+                print(
+                    f"Visited len after starting_word change: {len(visited)}, dfs_result_visited len: {len(dfs_result_visited)} ")
+                if guarding_value == 0:
+                    guarding_value = len(visited)
+                elif len(visited)<guarding_value:
+                    GAME_OVER = True
             else:
                 GAME_OVER = True
-                print(len(set(visited)))
+                print(f"Visited len after GAME_OVER: {len(visited)}")
 
     '''for node in all_words_dict:
         for edge in all_words_dict[node]:
